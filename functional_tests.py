a60e9e0da5bed22f1f0c06559a63bbfd9d4a2125
Michael Babyn <mike.babyn@gmail.com>
2016-11-15 09:17:08 -0500

    x

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         #new online to do list app. check out the homepage
@@ -38,10 +43,9 @@ class NewVisitorTest(unittest.TestCase):
         #the text boc to edit is still there after she presses enter
         #and the page updates
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
         #There is still a text box inviting her to add another item
         #enters use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
