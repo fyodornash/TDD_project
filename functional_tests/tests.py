1131f894d53ff9945294e5c3b918bbd90c160368
Michael Babyn <mike.babyn@gmail.com>
2016-11-16 10:52:32 -0500

    make functional tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary =FirefoxBinary(
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         #new online to do list app. check out the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         #the page title and header mention to do lists
         self.assertIn('To-Do', self.browser.title)
@@ -49,7 +50,7 @@ class NewVisitorTest(unittest.TestCase):
         #There is still a text box inviting her to add another item
         #enters use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('use peacock feathers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         
         #page updates and shows both items on the list
@@ -60,9 +61,6 @@ class NewVisitorTest(unittest.TestCase):
 
         #at the unique url already created her to do list is still there.
         self.fail('Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
 
 
 
