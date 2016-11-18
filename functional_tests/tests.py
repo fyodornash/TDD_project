ec2714b14408d7f8eed7c85b2c668495e50e5b68
Michael Babyn <mike.babyn@gmail.com>
2016-11-17 21:32:38 -0500

    first steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,28 @@ from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(LiveServerTestCase):
-
+    
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        
+        #she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta= 5
+        )
+        #she starts a list and notices that the input box is
+        #still nicely centered.
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta = 5
+        )
+    
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary =FirefoxBinary(
         firefox_path = '/home/michael/Downloads/firefox/firefox'))
