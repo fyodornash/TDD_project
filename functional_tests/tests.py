6c6e3de490c2e553c07469a0d29ba3b5c92a39f8
Michael Babyn <mike.babyn@gmail.com>
2016-11-19 16:38:26 -0500

    used Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
