9f2d146a3d6d17bd9c00b2ff590b2d878efa9dbe
Michael Babyn <mike.babyn@gmail.com>
2016-11-30 11:06:04 -0500

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         #edith goes to the home page and accidentally tries to submit
         self.browser.get(self.server_url)
-        self.self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
         #an empty list item. She hits enter on the empty input box
         
         #the home page refreshes, and there is an error message saying that the 
@@ -16,11 +16,11 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
         
         #she tries again with some text for the item which works
-        self.self.get_item_input_box().send_keys('Buy milk\n')
+        self.get_item_input_box().send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
         
         #perversely, she now decides to submit a second blank item
-        self.self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
         
         #she receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
@@ -28,7 +28,7 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
         
         #and she can correct it by filling some text in
-        self.self.get_item_input_box().send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
         
